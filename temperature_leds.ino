d9979f2130c42b46266496bae6c4f0aa14e5085d
Tomas Nilsson <tomas.nilsson78@gmail.com>
2019-12-20 11:17:15 +0100

    saved the .ino file

--- temperature_leds.ino
+++ temperature_leds.ino
@@ -1,1 +1,28 @@
+/* 	
+This program turns on a red LED if temperature detected
+on TMP36 sensor is greater than raw analog value 230 
+*/
+int analogSensorPin = A0;
+int tempSensorValue;
+int redLed = 4;
 
+
+void setup()
+{
+  pinMode(redLed = 4, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  tempSensorValue = analogRead(analogSensorPin);
+  Serial.println(tempSensorValue);
+  if(tempSensorValue>230)
+  {
+  	digitalWrite(redLed, HIGH);
+  }
+  else 
+  {
+  	digitalWrite(redLed, LOW);
+  }
+}
